84e6ff3abc521a9be7f98868946026ac0fb4fc0d
Your Name <natoli.missy@gmail.com>
2021-05-23 13:49:04 -0500

    change test folder name

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -6,5 +6,5 @@ test('creates an employee object', () => {
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
-}
+}  
 );
